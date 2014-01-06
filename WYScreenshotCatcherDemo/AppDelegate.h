2b9fd018f89eaf82418ce226831dc80461fdc4e3
William Yang <ghost126@gmail.com>
2014-01-06 18:25:30 +0800

    Initial Commit

---
+++ WYScreenshotCatcherDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WYScreenshotCatcherDemo
+//
+//  Created by William on 2014/1/6.
+//  Copyright (c) 2014年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WYScreenshotCatcherDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WYScreenshotCatcherDemo
+//
+//  Created by William on 2014/1/6.
+//  Copyright (c) 2014年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
